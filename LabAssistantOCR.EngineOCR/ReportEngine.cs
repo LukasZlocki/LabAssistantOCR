2d47e4b08d99dccc4a711e2ab97bebd9556f70c7
lzlocki <lzlocki@poczta.fm>
2024-11-03 19:26:43 +0100

    add empty project structure

---
+++ LabAssistantOCR.EngineOCR/ReportEngine.cs
@@ -0,0 +1,15 @@
+﻿namespace LabAssistantOCR.EngineOCR
+{
+    public class ReportEngine
+    {
+        public void LoadMachineReportImage(string pathToImage, string fileName)
+        {
+            // ToDo : Code loading img for further processing
+        }
+
+        public void GetReport()
+        {
+            // ToDo: Code retriving final report
+        }
+    }
+}
