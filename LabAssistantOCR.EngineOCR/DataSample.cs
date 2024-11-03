a48b8681079f629cdbcbd1d20e9253643668494c
lzlocki <lzlocki@poczta.fm>
2024-11-03 19:47:41 +0100

    add class to store extracted data

---
+++ LabAssistantOCR.EngineOCR/DataSample.cs
@@ -0,0 +1,20 @@
+﻿namespace LabAssistantOCR.EngineOCR
+{
+    public class DataSample
+    {
+        public string Date { get; set; }
+        public string um4 { get; set; }
+        public string um6 { get; set; }
+        public string um14 { get; set; }
+
+        public void ShowDataSample()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Report extracted sample:");
+            Console.WriteLine("Date : {0}", Date);
+            Console.WriteLine("4um  : {0}", um4);
+            Console.WriteLine("6um  : {0}", um6);
+            Console.WriteLine("14um : {0}", um14);
+        }
+    }
+}
