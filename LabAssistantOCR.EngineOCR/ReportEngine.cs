eb5cd733234dafa6c281fff59f0bb916cabc79f1
lzlocki <lzlocki@poczta.fm>
2024-11-04 19:35:32 +0100

    add laoding img

--- LabAssistantOCR.EngineOCR/ReportEngine.cs
+++ LabAssistantOCR.EngineOCR/ReportEngine.cs
@@ -1,11 +1,16 @@
-﻿namespace LabAssistantOCR.EngineOCR
+﻿using Tesseract;
+
+namespace LabAssistantOCR.EngineOCR
 {
     public class ReportEngine
     {
         public void ReadMachineReportImage(string pathToImage, string fileName)
         {
             // ToDo : Code loading img for further processing
+            var img = LoadImage(pathToImage, fileName);
+
             // ToDo : Code to pre process img
+
             // ToDo : Extract text from image
             // TODO : Clean data 
             // ToDo : Store data in final report
@@ -16,5 +21,22 @@
         {
             // ToDo: Code retriving final report
         }
+
+        private Pix LoadImage(string path, string filename)
+        {
+            try
+            {
+                Pix img = Pix.LoadFromFile(path + @"\" + filename);
+                return img;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Not able to load image " + ex.Message);
+                return null;
+            }
+        }
+
+
+
     }
 }
