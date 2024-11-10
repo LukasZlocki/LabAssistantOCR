448b76d3b6fcc1c4843f70710f0e9701ad45645b
lzlocki <lzlocki@poczta.fm>
2024-11-10 19:43:52 +0100

    add new window. add drag and drop area. add printing results area. add comunitation between main ocr engine

--- LabAssistantOCR/MainWindow.xaml.cs
+++ LabAssistantOCR/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
+﻿using LabAssistantOCR.EngineOCR;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LabAssistantOCR
 {
@@ -16,9 +8,42 @@ namespace LabAssistantOCR
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ReportEngine _reportEngine = new ReportEngine();
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void FileDropStackPanel_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                string filename = System.IO.Path.GetFileName(files[0]);
+                string path = System.IO.Path.GetFullPath(files[0]);
+                string path2 = System.IO.Path.GetDirectoryName(files[0]);
+
+                Show(path);
+            }
+        }
+
+        public void Show(string path)
+        {
+            string rawdata = GetRawDataFromImage(path);
+            ShowDataOnScreen(rawdata);
+        }
+
+        private string GetRawDataFromImage(string path)
+        {
+            return _reportEngine.GetTextFromImage_NoPreprocessing(path);
+        }
+
+        private void ShowDataOnScreen(string data)
+        {
+            txtScreen.Text = data;
+        }
+
+
     }
 }
