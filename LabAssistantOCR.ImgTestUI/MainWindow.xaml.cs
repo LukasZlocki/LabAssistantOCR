6c0ad716689519d26bb086f4b5f7841969ade73b
lzlocki <lzlocki@poczta.fm>
2024-12-22 17:47:32 +0100

    add logic to handle with ocr library

--- LabAssistantOCR.ImgTestUI/MainWindow.xaml.cs
+++ LabAssistantOCR.ImgTestUI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using LabAssistantOCR.EngineOCR;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +17,66 @@ namespace LabAssistantOCR.ImgTestUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ReportEngine _reportEngine;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void FileDropStackPanel_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                string filename = System.IO.Path.GetFileName(files[0]);
+                string path = System.IO.Path.GetFullPath(files[0]);
+                string path2 = System.IO.Path.GetDirectoryName(files[0]);
+
+                Show(path);
+            }
+        }
+
+        private void Show(string path)
+        {
+            // Gettting raw data from image without preprocessing
+            string rawdata = GetRawDataFromImage(path);
+            ShowDataOnScreen(rawdata);
+
+            // Get report from image without preprocessing
+            DataSample dataSample = new DataSample();
+            dataSample = GetDatasampleFromImage(path);
+            ShowReportOnScreen(dataSample);
+
+        }
+
+        private string GetRawDataFromImage(string path)
+        {
+            _reportEngine = new();
+            return _reportEngine.GetTextFromImage_NoPreprocessing(path);
+        }
+
+        private DataSample GetDatasampleFromImage(string path)
+        {
+            return _reportEngine.GetReportFromImage_NoPreProcessing(path);
+        }
+
+        private void ShowDataOnScreen(string data)
+        {
+            txtScreen.Text = string.Empty;
+            txtScreen.Text = data;
+        }
+
+        private void ShowReportOnScreen(DataSample dataSample)
+        {
+            txtReportScreen.Text = string.Empty;
+            string report = "Report date: " + dataSample.Date + "\n"
+                + " 4um  : " + dataSample.um4 + "\n"
+                + " 6um  : " + dataSample.um6 + "\n"
+                + " 14um : " + dataSample.um14;
+            txtReportScreen.Text = report;
+        }
+
+
     }
 }
